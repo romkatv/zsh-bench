6d475c86d0124aad2657b8e6d255ab4f7b620ff5
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2021-10-16 07:50:36 +0200

    add --keep to zsh-bench to simplify debugging

--- internal/benchmark-all.zsh
+++ internal/benchmark-all.zsh
@@ -11,34 +11,25 @@ local -r root_dir=${ZSH_SCRIPT:h:h}
 
 zmodload zsh/zutil
 
-local -a iters
-zparseopts -D -K -F -- {i,-iters}:=iters
-
-function cleanup() {
-  userdel -rf $user 2>/dev/null || true
-  rm -rf -- '' /tmp/*(ND)
-}
-
-cleanup
+local -a flags
+zparseopts -D -K -F -a flags -- {k,-keep} {i,-iters}:
 
 local cfg
 for cfg; do
-  {
-    useradd -ms /bin/zsh $user
-    cp -r -- ~/.terminfo /home/$user/
-    cp -r -- $root_dir /home/$user/zsh-bench
-    chown -R $user:$user /home/$user/{zsh-bench,.terminfo}
-    local cmd=(
-      'export LC_ALL='${(qqq)LC_ALL}
-      'cd -q -- ~/zsh-bench/configs/'$cfg
-      './setup'
-      'cd -q'
-      '~/zsh-bench/zsh-bench '${(j: :)${(@qqq)iters}})
-    print -r -- "==> benchmarking $cfg ..."
-    sudo -u $user zsh -fc ${(j: && :)cmd}
-  } always {
-    cleanup
-  }
+  userdel -rf $user 2>/dev/null || true
+  rm -rf -- '' /tmp/*(ND)
+  useradd -ms /bin/zsh $user
+  cp -r -- ~/.terminfo /home/$user/
+  cp -r -- $root_dir /home/$user/zsh-bench
+  chown -R $user:$user /home/$user/{zsh-bench,.terminfo}
+  local cmd=(
+    'export LC_ALL='${(qqq)LC_ALL}
+    'cd -q -- ~/zsh-bench/configs/'${(qqq)cfg}
+    './setup'
+    'cd -q'
+    '~/zsh-bench/zsh-bench '${(j: :)${(@qqq)flags}})
+  print -r -- "==> benchmarking $cfg ..."
+  sudo -u $user zsh -fc ${(j: && :)cmd}
 done
 
 } "$@"
